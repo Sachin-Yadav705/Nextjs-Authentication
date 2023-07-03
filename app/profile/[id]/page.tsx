bba96c6a2afbce59745866ba0e6836bc6c6f50db
Sachin Kumar <Kumar9891sachin@gmail.com>
2023-07-04 01:55:49 +0530

    day-1

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,17 @@
+export default function UserProfile({ params }: any) {
+  return (
+    <div
+      className="flex flex-col items-center
+        justify-center min-h-screen py-2"
+    >
+      <h3>Profile</h3>
+      <hr />
+      <p className="text-4xl">
+        Profile Page
+        <span className="p-2 rounded bg-orange-500 text-black">
+          {params.id}
+        </span>
+      </p>
+    </div>
+  );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,12 @@
+export default function ProfilePage() {
+  return (
+    <div
+      className="flex flex-col items-center
+        justify-center min-h-screen py-2"
+    >
+      <h3>Profile</h3>
+      <hr />
+      <p>Profile Page</p>
+    </div>
+  );
+}
